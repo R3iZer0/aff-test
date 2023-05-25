a5a3c78edb8d7fb5e38a51f17306c4af13534abf
R3iZer0 <94191193+R3iZer0@users.noreply.github.com>
2023-05-25 18:11:55 +0200

    sound work *but refreshes infinitely to 5sec

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from registration.views import download_table, home, lead_list, lead_register, leads_data_view
+from registration.views import download_table, home, lead_list, lead_register, leads_data_view,save_to_google_sheet
 urlpatterns = [
     path('', lead_register, name='registration'),  # change the root path to lead_register
     path('home/', home, name='home'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('leads_view/', lead_list, name='leads_view'),
     path('leads_data/', leads_data_view, name='leads_data'),
     path('download_table/', download_table, name='download_table'),
+    path('save-to-google-sheet/', save_to_google_sheet, name='save_to_google_sheet'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -3,9 +3,16 @@ from django.urls import path
 from django.shortcuts import redirect, render
 from .forms import LeadForm
 from .models import Lead
-from django.http import HttpResponse, request
+from django.http import HttpRequest, HttpResponse, request
 from django.http import JsonResponse
 from openpyxl import Workbook
+import os
+import datetime
+import pytz
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from django.shortcuts import render
+from .models import Lead
 
 def home(request):
     return render(request, 'home.html', {})
@@ -77,3 +84,47 @@ def download_table(request):
     workbook.save(response)
     
     return response
+
+
+
+
+
+def save_to_google_sheet(request: HttpRequest):
+    # Path to the service account key JSON file
+    key_file_path = 'registration/static/css/i-informatics-387814-d693dbfa6c95.json'
+
+    # ID of the Google Sheet to be updated
+    sheet_id = '13PQeiimRhhPBJ86eUSPMUpNAFQh0-zB0Lkrq1CzsPZo'
+
+    # Connect to Google Sheets API using the service account key
+    credentials = service_account.Credentials.from_service_account_file(key_file_path)
+    service = build('sheets', 'v4', credentials=credentials)
+
+    # Get the current date and time in UTC
+    current_datetime = datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')
+
+    # Retrieve the table data from the database
+    leads = Lead.objects.all()
+
+    # Prepare the data to be written to the Google Sheet
+    data = []
+    for lead in leads:
+        data.append([lead.name, lead.last_name, lead.email, lead.phone, lead.country, lead.get_experience_display(), current_datetime])
+
+    # Define the range where the data should be written (e.g., Sheet1!A1:G)
+    range_name = 'Sheet1!A1:G'
+
+    # Construct the request to update the Google Sheet
+    value_range_body = {
+        'values': data
+    }
+    request = service.spreadsheets().values().update(
+        spreadsheetId=sheet_id,
+        range=range_name,
+        valueInputOption='RAW',
+        body=value_range_body
+    )
+    response = request.execute()
+
+    # Render a response to the user
+    return render(request, 'leads_view.html')
